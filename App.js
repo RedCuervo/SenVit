63ce995e58652c0a173d642c64cfd2533d3183de
Alan Guevara (Lau) <152565322+Laudrino@users.noreply.github.com>
2024-12-06 00:29:24 -0600

    Add files via upload

--- App.js
+++ App.js
@@ -1,32 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { NativeBaseProvider, theme, VStack } from 'native-base';
+import { NativeBaseProvider } from 'native-base';
 import { createStackNavigator } from '@react-navigation/stack';
+import { AuthProvider } from './src/context/AuthContext'; // Importa el AuthProvider
 
+// pantallas
 import HomeScreenNoDevice from './src/screens/HomeScreenNoDevice';
 import FirstTimeScreen from './src/screens/FirstTimeScreen';
 import ConnectionScreen from './src/screens/ConnectionScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
+import LoginScreen from './src/screens/LoginScreen';
+
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <NativeBaseProvider backgroundColor="white">
-      <NavigationContainer>
-        <VStack flex={1} bg={"white"}>
-        <Stack.Navigator>
-          <Stack.Screen name="FirstScreen" component={FirstTimeScreen} />
-          <Stack.Screen name="HomeScreen1" component={HomeScreenNoDevice} />
-          <Stack.Screen name="ConnectionScreen" component={ConnectionScreen} options={{headerShown: false}}/>
-        </Stack.Navigator>
-        </VStack>
-      </NavigationContainer>
+   /* envuelve el componente raiz para que funcione la autenticacion*/
+    <AuthProvider> 
+      <NativeBaseProvider>
+        <View style={styles.container}>
+          <NavigationContainer>
+            <Stack.Navigator
+              initialRouteName="FirstScreen" // Define la pantalla inicial
+              screenOptions={{
+                headerShown: false, // Oculta el encabezado globalmente
+              }}
+            >
+              <Stack.Screen name="FirstScreen" component={FirstTimeScreen} />
+              <Stack.Screen name="HomeScreen1" component={HomeScreenNoDevice} />
+              <Stack.Screen name="ConnectionScreen" component={ConnectionScreen} />
+              <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+              <Stack.Screen name="LoginScreen" component={LoginScreen} />
+            </Stack.Navigator>
+          </NavigationContainer>
+          <StatusBar style="auto" />
+        </View>
       </NativeBaseProvider>
-      <StatusBar style="auto" />
-    </View>
+    </AuthProvider>
   );
-}
+};
+
 export default App;
 
 const styles = StyleSheet.create({
